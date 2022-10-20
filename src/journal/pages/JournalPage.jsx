2e59358c525525c5ec71d0a74fa005c884115fe5
Nicolas Quinn <nclsquinn@gmail.com>
2022-10-20 12:53:18 -0300

    Class 238 - MUI config with Vite

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,8 @@
+import { Typography } from "@mui/material"
 
 
 export const JournalPage = () => {
   return (
-    <h1>JournalPage</h1>
+    <Typography variant="h1">JournalPage</Typography>
   )
 }
